0e37f6209ab88496c2b6d6ef5b4f6152e25449c6
akshaysl-ace <akshaysl3867@gmail.com>
2022-12-15 23:41:02 +0530

    added pages and related components in client

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
+
+const Home = () => {
+  return (
+    <>
+      <section className='heading'>
+        <h2>What do you need help with ?</h2>
+        <p>Please choose from the options below :</p>
+      </section>
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create a New Ticket ..
+      </Link>
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View My Tickets
+      </Link>
+    </>
+  );
+};
+
+export default Home;
